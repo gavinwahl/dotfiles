6fe53e48c33020a0a5743ee7147d8283ebf00d63
Gavin Wahl <gwahl@fusionbox.com>
2013-02-27 14:29:32 -0700

    django aliases

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -6,3 +6,10 @@ alias w=workon
 alias tags="ctags '--exclude=*.min.*' --exclude='*/CACHE/*' -R"
 alias gu='git rev-parse --show-toplevel > /dev/null && cd `git rev-parse --show-toplevel`'
 alias mp='find -type f -iregex ".*\.\(mp3\|wmv\|mp4\|mkv\|flac\|avi\|iso\|vob\)$" -exec mplayer -cache 1000 {} +'
+alias ma="python ./manage.py"
+alias mash="ma shell_plus"
+alias mars="ma runserver"
+alias marsp="ma runserver_plus"
+alias mag="ma migrate"
+alias mam="ma schemamigration --auto"
+alias rmpyc="find -name '*.pyc' -delete"
