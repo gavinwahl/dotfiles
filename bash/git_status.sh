c3d7e099eb77c394ba7f9d7df4e63675a35a8b30
Gavin Wahl <gwahl@fusionbox.com>
2014-01-09 12:10:09 -0700

    optimize git_status

--- bash/git_status.sh
+++ bash/git_status.sh
@@ -1,7 +1,8 @@
 git_status()
 {
-  not_a_git=$(g status 2>&1 | grep 'Not a git' 2> /dev/null)
-  is_clean=$(g status 2>&1 | grep 'nothing to commit' 2> /dev/null)
+  status=$(g status 2>&1)
+  not_a_git=$(echo $status | grep 'Not a git' 2> /dev/null)
+  is_clean=$(echo $status | grep 'nothing to commit' 2> /dev/null)
   if [ -z "$not_a_git" -a -z "$is_clean" ]; then
     echo -ne "*"
   fi
