712051536111c641c57d72103006b8e2d270916a
Gavin Wahl <gwahl@fusionbox.com>
2014-01-16 15:41:18 -0700

    don't put git status in the prompt over sshfs!

--- bash/git_status.sh
+++ bash/git_status.sh
@@ -1,5 +1,10 @@
 git_status()
 {
+  # check for sshfs, way too slow otherwise
+  if stat -f -c "%T" . | grep 'fuseblk' >/dev/null
+  then
+    return
+  fi
   status=$(g status 2>&1)
   not_a_git=$(echo $status | grep 'Not a git' 2> /dev/null)
   is_clean=$(echo $status | grep 'nothing to commit' 2> /dev/null)
